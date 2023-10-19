45bf77e9eb1c005eff73b131a1b5c70952c9a133
BimalKanxa <sharmabimal2003@gmail.com>
2023-10-19 14:08:23 +0530

    crud completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,114 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
+// Hooks
+import { useState, useEffect } from 'react';
+// Packages
+import { v4 as uuidv4 } from 'uuid';
+import Swal from "sweetalert2";
+
 
 function App() {
+
+  const [tasks, setTasks] = useState([]); // Task State
+const [showAddTask, setShowAddTask] = useState(false); // To reveal add task form
+const [loading, setloading] = useState(true);
+
+useEffect(() => {
+  setTimeout(() => {
+      setloading(false); 
+  }, 1000);
+
+}, [])
+
+//create task
+const addTask = (task) => {
+  const id = uuidv4();
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask]);
+  Swal.fire({
+      icon: 'success',
+      title: 'Yay...',
+      text: 'You have successfully added a new task!'
+  })
+  localStorage.setItem("taskAdded", JSON.stringify([...tasks, newTask]));
+}
+
+// Delete Task
+const deleteTask = (id) => {
+  const deleteTask = tasks.filter((task) => task.id !== id);
+  setTasks(deleteTask);
+  Swal.fire({
+      icon: 'success',
+      title: 'Oops...',
+      text: 'You have successfully deleted a task!'
+  })
+  localStorage.setItem("taskAdded", JSON.stringify(deleteTask));
+}
+
+// Edit Task
+const editTask = (id) => {
+  const text = prompt("Task Name");
+  const day = prompt("Description");
+  let data = JSON.parse(localStorage.getItem('taskAdded'));
+  const myData = data.map(x => {
+      if (x.id === id) {
+          return {
+              ...x,
+              text: text,
+              day: day,
+              id: uuidv4()
+          }
+      }
+      return x;
+  })
+  Swal.fire({
+      icon: 'success',
+      title: 'Yay...',
+      text: 'You have successfully edited an existing task!'
+  })
+  localStorage.setItem("taskAdded", JSON.stringify(myData));
+    window.location.reload();
+}
+
+// Fetching from Local Storage
+const getTasks = JSON.parse(localStorage.getItem("taskAdded"));
+useEffect(() => {
+    if (getTasks == null) {
+        setTasks([])
+    } else {
+        setTasks(getTasks);
+    }
+      //eslint-disable-next-line
+}, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+  { loading? 
+
+  ( <div className='spinner-container'><div className="spinner-border" role="status">
+  <span className="visually-hidden">Loading...</span></div> 
+      </div>
+  ): 
+  <div className='container'>
+   <Header showForm={() => setShowAddTask(!showAddTask)} changeTextAndColor={showAddTask} />
+    {/* Revealing the Add Task Form */}
+    {showAddTask && <AddTask onSave={addTask} />}
+            
+            {/* Displaying Tasks */}
+            {
+              tasks.length > 0 ?
+                (<Tasks tasks={tasks} onDelete={deleteTask} onEdit={editTask}/>) :
+                ('No Task Found!')
+            }
+{/* Task Counter */}
+<h3>Number of Tasks: {tasks.length}</h3>
+   </div>
+   }
+    </>
   );
 }
 
---
+++ src/components/AddTask.jsx
@@ -0,0 +1,53 @@
+import React, {useState} from 'react'
+import Swal from "sweetalert2"
+
+const AddTask = ({onSave}) => {
+    const [text, setText] = useState('');
+  const [day, setDay] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (!text && !day) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Fill in your task and description or close the form!'
+        })
+    } else if (!text && day) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Fill in your task!'
+        })
+    } else if (text && !day) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Fill in your description!'
+        })
+    } else {
+        onSave({ text, day });
+    }
+    
+    setText('');
+    setDay('');
+}
+
+  return (
+    <div>
+ <form className="add-form" onSubmit={onSubmit}>
+          <div className="form-control">
+              <label>Task Title</label>
+              <input type="text" placeholder="add task" value={text} onChange={(e) => setText(e.target.value)} />
+          </div>
+          <div className="form-control">
+              <label>Description </label>
+              <input type="text" placeholder="description" value={day} onChange={(e) => setDay(e.target.value)} />
+          </div>
+          <input type="submit" className="btn btn-block" value="Save Task" />
+      </form>
+    </div>
+  )
+}
+
+export default AddTask
